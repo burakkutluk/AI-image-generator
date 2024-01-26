a7dd169622238889cadf313076eda6dc9f79a158
19691473550 <burakkutlukk@gmail.com>
2024-01-26 16:07:57 +0300

    server

--- server/index.js
+++ server/index.js
@@ -1,26 +1,31 @@
-import express from "express"
-import * as dotenv from "dotenv"
-import cors from "cors"
-import connectDB from "./mongoDB/connect.js"
+import express from "express";
+import * as dotenv from "dotenv";
+import cors from "cors";
 
-dotenv.config()
-const PORT = process.env.PORT
+import connectDB from "./mongodb/connect.js";
+import postRoutes from "./routes/postRoutes.js";
+import dalleRoutes from "./routes/dalleRoutes.js";
 
-const app = express()
-app.use(cors())
-app.use(express.json({limit:'50mb'}))
+dotenv.config();
+const port = process.envPORT
 
-app.get('/', async (req, res) => {
-    res.send('Hello from AImag!')
-})
+const app = express();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+
+app.get("/", async (req, res) => {
+  res.status(200).json({
+    message: "Hello from DALL.E!",
+  });
+});
 
 const startServer = async () => {
-    try {
-      connectDB(process.env.MONGODB_URL);
-      app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
-    } catch (error) {
-      console.log(error);
-    }
-  };
-  
-  startServer();
+  try {
+    connectDB(process.env.MONGODB_URL);
+    app.listen(port, () => console.log(`Server started on port ${port}`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+startServer();
